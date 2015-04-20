32cf067cfacd2dd9e2598e113228a3c5a0be16d0
David <dflores0818@gmail.com>
2015-04-19 20:40:34 -0700

    updating files, file paths should now work with a wider range of working directories.  also increased effiecency in the paintComponent() method.

--- AnimationTest/src/animationtest/FTCauto.java
+++ AnimationTest/src/animationtest/FTCauto.java
@@ -8,7 +8,7 @@ import javax.swing.*;
 public class FTCauto extends JFrame {
     
     public FTCauto() {
-        add(new MovingMessagePanel("message moving"));
+        add(new MovingMessagePanel());
     }
     
     public static void main(String[] args) {
@@ -38,11 +38,11 @@ public class FTCauto extends JFrame {
         private int frames =0;
         
         //Picture stuff
-        private static final Image stuffedGriffins = new ImageIcon("STUFFED_GRIFFINS_FINAL_GRN.png").getImage();
-        private static final Image field = new ImageIcon("Field.png").getImage();
-        private static final Image field_shadow = new ImageIcon("Field_Shadow.png").getImage();
+        private static final Image stuffedGriffins = Toolkit.getDefaultToolkit().getImage("AnimationTest\\STUFFED_GRIFFINS_FINAL_GRN.jpg");
+        private static final Image field = Toolkit.getDefaultToolkit().getImage("AnimationTest\\Field.png");
+        private static final Image field_shadow = Toolkit.getDefaultToolkit().getImage("AnimationTest\\Field_Shadow.png");
         
-        public MovingMessagePanel(String message){
+        public MovingMessagePanel(){
             
             
             //Getting mouse location when moved
@@ -64,79 +64,67 @@ public class FTCauto extends JFrame {
 
         protected void paintComponent(Graphics g){
             super.paintComponent(g);
-            
-            //Main program
-            Color darkBlueD = new Color(45,62,78);
-            Color darkBlueL = new Color(52,74,97);
-            Color lightGreenD = new Color(23,160,134);
-            Color lightGreenL = new Color(27,188,155);
-            
-            Color lightGreenLT = new Color(27,188,155,100);
-            
-            
-            //Main frame
-            g.setColor(darkBlueL);
-            g.fillRect(0, 0, getWidth(), getHeight());
-            g.setColor(darkBlueD);
-            g.fillRect(0, 0, 110, getHeight());
 
-            g.setColor(lightGreenD);
-            g.fillRect(0, 0, 100, getHeight());
-            
-            g.setColor(lightGreenL);
-            g.fillRect(0, 0, 5, getHeight());
-            
-            g.fillRect(0, 0, 100, 5);
-            
-            //Mouse stuff
-            //g.drawLine(mouseX, 0, mouseX, getHeight());
-            //g.drawLine(0, mouseY, getWidth(), mouseY);
-            
-            //g.drawLine(mouseX+1, 0, mouseX+1, getHeight());
-            //g.drawLine(0, mouseY+1, getWidth(), mouseY+1);
-            
-                        //Resizing the field
-            if(getWidth()-100<getHeight()){
-                fieldSize = getWidth()-100;
-            }else{
-                fieldSize = getHeight()-10;
+            //Resizing the field
+            if (getWidth() - 100 < getHeight()) {
+                fieldSize = getWidth() - 100;
+            } else {
+                fieldSize = getHeight() - 10;
             }
-            
-            //Field
-            g.drawImage(field, 100, 10, fieldSize, fieldSize,null);
-            
-            //----Shadow Testing (having a semi-transparent image cast "shadows" onto the field)----
-            //g.setColor(lightGreenL);
-            //g.fillRect(mouseX, mouseY, 100, 100);
-            
-            //Field shadow
-            //g.drawImage(field_shadow, 100, 10, fieldSize, fieldSize,null);
-            //----------------------
-            
-            //Opening credits & stuff
-            if(openingTrans>0&&frames>200){
-                openingTrans-=5;
+
+            if(frames > 300) {
+                //Main program
+                Color darkBlueD = new Color(45, 62, 78);
+                Color darkBlueL = new Color(52, 74, 97);
+                Color lightGreenD = new Color(23, 160, 134);
+                Color lightGreenL = new Color(27, 188, 155);
+                Color lightGreenLT = new Color(27, 188, 155, 100);
+
+
+                //Main frame
+                g.setColor(darkBlueL);
+                g.fillRect(0, 0, getWidth(), getHeight());
+                g.setColor(darkBlueD);
+                g.fillRect(0, 0, 110, getHeight());
+
+                g.setColor(lightGreenD);
+                g.fillRect(0, 0, 100, getHeight());
+
+                g.setColor(lightGreenL);
+                g.fillRect(0, 0, 5, getHeight());
+
+                g.fillRect(0, 0, 100, 5);
+
+                //Mouse stuff
+                g.drawLine(mouseX, 0, mouseX, getHeight());
+                g.drawLine(0, mouseY, getWidth(), mouseY);
+
+                g.drawLine(mouseX + 1, 0, mouseX + 1, getHeight());
+                g.drawLine(0, mouseY + 1, getWidth(), mouseY + 1);
+
+                //Field
+                g.drawImage(field, 100, 10, fieldSize, fieldSize, null);
+
+                //----Shadow Testing (having a semi-transparent image cast "shadows" onto the field)----
+                //g.setColor(lightGreenL);
+                //g.fillRect(mouseX, mouseY, 100, 100);
+
+                //Field shadow
+                //g.drawImage(field_shadow, 100, 10, fieldSize, fieldSize,null);
+                //----------------------
+            } else {
+                //Opening credits & stuff
+                if (openingTrans > 0 && frames > 50) {
+                    openingTrans--;
+                }
+
+
+                Color OpeningBackground = new Color(50, 50, 50, openingTrans);
+                g.setColor(OpeningBackground);
+                g.fillRect(0, 0, getWidth(), getHeight());
+
+                g.drawImage(stuffedGriffins, (getWidth() / 2) - 374, (getHeight() / 2) - 149, 748, 299, null);
             }
-            
-            if(frames>50 && openingTextTrans>0){
-                openingTextTrans-=5;
-            }else if(openingTextTrans>0&&frames>100){
-                
-            }
-            
-            Color OpeningBackground = new Color(50,50,50,openingTrans);
-            g.setColor(OpeningBackground);
-            g.fillRect(0, 0, getWidth(), getHeight());
-            
-            if(frames<300){
-                g.drawImage(stuffedGriffins, (getWidth()/2)-374, (getHeight()/2)-149, 748, 299,null);
-            }
-            
-            
-            OpeningBackground = new Color(50,50,50,openingTextTrans);
-            g.setColor(OpeningBackground);
-            g.fillRect(0, 0, getWidth(), getHeight());
-            
             
             frames++;
         }
