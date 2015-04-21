28841efa2be609d392cd3a6646ec0babdbf823cc
thomas <thomas@localhost>
2015-04-21 04:36:20 -0700

    Fixed the placement and added point animaton. :)

--- AnimationTest/src/animationtest/Point.java
+++ AnimationTest/src/animationtest/Point.java
@@ -7,7 +7,9 @@ public class Point {
     private double x = 10;
     private double y = 10;
     private int speed = 50;
-    
+    public int size = 100;
+    public int sizeSpeed = 1;
+    public int transparency = 1;
     
     //public int getScale
     
@@ -26,11 +28,11 @@ public class Point {
     }
     
     public void setX(int x) {
-        this.x = x  *( fieldSize/1000);
+        this.x = (x-100)  /( fieldSize/1000);
     }
     
     public void setY(int y) {
-        this.y = y *( fieldSize/1000);
+        this.y = (y-10) /( fieldSize/1000);
     }
     
     public void setSpeed(int speed) {
